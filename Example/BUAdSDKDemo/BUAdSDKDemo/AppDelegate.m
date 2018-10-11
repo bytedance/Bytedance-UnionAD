f7d284a7f6550ff2bbc296984c24ed32571452dc
李盛 <you@example.com>
2018-10-11 18:29:10 +0800

    fix space

--- Example/BUAdSDKDemo/BUAdSDKDemo/AppDelegate.m
+++ Example/BUAdSDKDemo/BUAdSDKDemo/AppDelegate.m
@@ -18,7 +18,6 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
